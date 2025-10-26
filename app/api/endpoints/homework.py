430e6ea255022675a5d094561960a89d8d3de73b
Oleksii Postovyi <a.postovoy918@gmail.com>
2025-10-26 19:08:38 +0200

    feat: add API routers

---
+++ app/api/endpoints/homework.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/homework", tags=["Homework"])
---
+++ app/api/endpoints/mind_map.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/mind-map", tags=["Mind Map"])
